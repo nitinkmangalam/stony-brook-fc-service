3e986554a706772aa72addb8da9ef9e75993ad55
Nitin Kumar Mangalam <nkmvicky@gmail.com>
2025-08-16 13:17:32 -0700

    Allow vercel in CORS

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://stony-brook-fc-web.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
